9f2575809bcb37bf2ca3b0342699e484552e1a44
alkolhar <alexkoller90@gmail.com>
2023-10-22 20:21:51 +0200

    add tests

--- src/test/java/com/example/tasker/TaskerApplicationTests.java
+++ src/test/java/com/example/tasker/TaskerApplicationTests.java
@@ -1,15 +1,60 @@
 package com.example.tasker;
 
-import com.example.tasker.account.AccountController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class TaskerApplicationTests {
+    private final static String ACCOUNT_ENDPOINT = "/api/accounts";
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldCreateAccount() throws Exception {
+        mockMvc.perform(post(ACCOUNT_ENDPOINT)
+                        .content("{" +
+                                "\"name\": \"Name\"," +
+                                "\"firstName\": \"FirstName\"," +
+                                "\"login\": \"Login2\"," +
+                                "\"email\": \"email@mail.ch\"" +
+                                "}")
+                        .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void shouldFindAccountWithValidId() throws Exception {
+        mockMvc.perform(get(ACCOUNT_ENDPOINT + "/{id}", 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Name"))
+                .andExpect(jsonPath("$.firstName").value("FirstName"))
+                .andExpect(jsonPath("$.login").value("Login2"))
+                .andExpect(jsonPath("$.email").value("email@mail.ch"))
+                .andReturn();
+    }
+
+    @Test
+    void shouldFindAccountWithValidSearchText() throws Exception {
+        mockMvc.perform(get(ACCOUNT_ENDPOINT + "/search")
+                        .param("searchText", "ogin"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+    }
 }
