0daa5a4bea25921c1a5ce6133b8699ed0231352f
alkolhar <alexkoller90@gmail.com>
2023-10-25 20:08:00 +0200

    add mapstruct

--- src/main/java/com/example/tasker/domain/BaseAbstractEntity.java
+++ src/main/java/com/example/tasker/domain/BaseAbstractEntity.java
@@ -30,15 +30,31 @@ public abstract class BaseAbstractEntity {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public Long getVersion() {
         return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
     }
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public LocalDateTime getModifiedAt() {
         return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
     }
 }
